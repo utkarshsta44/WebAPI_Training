cc1ab3e677839e110304bafbeae71cc590c449de
utkarshsta44 <utkarsh.srivastava@successive.tech>
2023-12-18 17:48:20 +0530

    Final commit Web API

---
+++ Pizza.cs
@@ -0,0 +1,10 @@
+﻿namespace collegeApi
+{
+   
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsGlutenFree { get; set; }
+    }
+}
